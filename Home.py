1ab7acae920a24b2a64ad7213ac29dc8618f1130
Miika Myötänen <miika.myotanen@hotmail.com>
2023-05-10 13:04:30 +0300

    add desc on the new columns

--- Home.py
+++ Home.py
@@ -5,14 +5,18 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/linkedin.jpg", width=300)
+    st.image("images/linkedin.jpg", width=400)
 
 with col2:
     st.title("Miika Myötänen")
-    content = """This is my portfolio webpage, consisting of my Python projects
+    content = """
+    This is my portfolio webpage, consisting of my Python projects
     from various coding courses and some of my own ideas, website is made
-     with only Python!"""
+     with only Python!
+     """
     st.info(content)
+
+st.write("Below you can find some of the apps built in Python. Feel free to contact me!")
 
 
 
